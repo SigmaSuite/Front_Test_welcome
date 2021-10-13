200e25eb7e1f7a65ec8cb57e70fe54c472b6f5ec
Bartosz Sendek <bartosz.sendek@pl.abb.com>
2021-10-14 00:30:06 +0200

    kickoff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -6,6 +7,9 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'},
+  ]
 })
 export class AppRoutingModule { }
